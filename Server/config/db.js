6c955ead203a47fc73a80ecf116b567d0f55b2fa
shivtherumbler <shivtherumbler@gmail.com>
2025-04-04 19:40:16 -0400

    db update

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,10 +1,7 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const client = new MongoClient(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const client = new MongoClient(process.env.MONGO_URI); // Remove deprecated options
 
 let db;
 
